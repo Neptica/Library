85b911b4b81afec5f53b7218c35f71ba7c98e8f9
Michael Mumme <michaelmumme@me.com>
2024-11-02 18:10:13 -0500

    first commit

---
+++ library.js
@@ -0,0 +1,41 @@
+const myLibrary = [];
+
+function Book(name, author, date) {
+  this.name = name;
+  this.author = author;
+  this.date = date;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = new Book("Yes", "Queen", "1955");
+const book2 = new Book("King Kong", "Tolkein", "1459");
+const book3 = new Book("Future", "JJR", "4857");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+const list = document.getElementById("list");
+
+console.log(book1.name);
+
+let created_doms = [];
+
+for (const book of myLibrary) {
+  let title = document.createElement("h3");
+  let author = document.createElement("p");
+  let date = document.createElement("p");
+  title.textContent = book.name;
+  author.textContent = book.author;
+  date.textContent = book.date;
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(date);
+  list.appendChild(card);
+  created_doms.push(card);
+}
